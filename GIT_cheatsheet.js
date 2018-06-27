f8cd5dd0cc24e5e15df2fd9f643351c74d9e3749
Raye <web24.red@gmail.com>
2018-06-26 22:23:19 -0700

    Local Version Control, and IGNORE

--- GIT_cheatsheet.js
+++ GIT_cheatsheet.js
@@ -23,3 +23,24 @@
 	ssh-add -K 					//adds to your private key to Mac Keychain
 	cat .ssh/id_rsa.pub 		// Display your public key
 	Tripple click to select... copy... add to GIT/profile/
+
+
+  /* 		GIT
+  =======================================================================
+  	Git is a local file-sharing and version control language
+
+  	GitHUB is a service that will store your projects (requires SSH)
+  		... all public accounts, private costs money
+  	BitBucket is another service
+  		... less than 5 members in a group for free.. over that pay
+  =======================================================================
+  */
+
+  /* 	GIT IGNORE
+  =======================================================================
+  	You probably dont want to share executeables and Thumbnails
+  	setup a .gitignore file with content from XXXXX ...
+  	contains a list of executables that should not be run
+  =======================================================================
+  */
+  	.gitignore
